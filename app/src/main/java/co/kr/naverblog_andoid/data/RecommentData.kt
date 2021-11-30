c3290ef3d49f2280e756d2aa0b83f7cde553c64c
Daehoww <kdh2769@gmail.com>
2021-12-01 02:47:41 +0900

    전체 UI 구현

--- app/src/main/java/co/kr/naverblog_andoid/data/RecommentData.kt
+++ app/src/main/java/co/kr/naverblog_andoid/data/RecommentData.kt
@@ -1,12 +1,13 @@
-package co.kr.naverblog_andoid.view.comment
+package co.kr.naverblog_andoid.data
 
 data class RecommentData(
     val id : Int,
     val name : String,
     val imageUrl : Int,
     val comment : String,
+    val isOwner : Boolean,
     val date : String,
     val time : String,
     val isHeartClick : Boolean,
     val heartCount : Int,
-    )
+)
